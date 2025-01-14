22975cef006d05a519aa130acaa0881bcf57dcb0
maltayar-1 <maltayar@amazon.com>
2025-01-14 11:07:11 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_wfAY0vgM5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1c2up923jkqjhnq682toude548', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_NfBGrdTM5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7562lqvll6ptmjfpn3t0e76oj3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
